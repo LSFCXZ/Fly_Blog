d2fda3bce787aff13da0b3b63c689469cf41cebe
Lin <1770813141@qq.com>
2021-04-07 22:53:09 +0800

    增加了头部组件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div id="app">
-
+    <lsf-header></lsf-header>
     <router-view />
   </div>
 </template>
-
+<script>
+import Header from '@/components/Header.vue'
+export default {
+  name: 'App',
+  components: {
+    'lsf-header': Header
+  }
+}
+</script>
 <style lang="scss">
+@import "assets/css/global.css";
 </style>
