3a3a574acef07037fcb45a0f4807092e73082e2d
Lin <1770813141@qq.com>
2021-04-27 16:26:30 +0800

    update log

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="fly-footer">
     <p><a href="javascript:void()"
-        target="_blank">Lay社区</a> 2021 &copy; <a href="javascript:void(0)"
+        target="_blank">LryBlog社区</a> 2021 &copy; <a href="javascript:void(0)"
         target="_blank">blog.frontblog.top 出品</a></p>
     <p>
+      <a href="https://gitee.com/lsfcxz/frontblog"
+        target="_blank">开源地址</a>
       <a href="javascript:void(0)"
-        target="_blank">付费计划</a>
-      <a href="javascript:void(0)"
-        target="_blank">获取Lay社区模版</a>
+        target="_blank">获取LryBlog社区模版</a>
       <a href="javascript:void(0)"
         @mouseover="show()"
         @mouseout="hide()"
